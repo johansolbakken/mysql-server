3d785b5310314b679d365818505e1a38827c2b90
Johan Solbakken <johan.hvaler@gmail.com>
2025-01-30 17:08:47 +0100

    oohj: remove unused import

--- sql/iterators/hash_join_iterator.cc
+++ sql/iterators/hash_join_iterator.cc
@@ -22,7 +22,6 @@
    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA */
 
 #include "sql/iterators/hash_join_iterator.h"
-#include "sql/iterators/optimistic_hash_join_iterator.h"
 
 #include <algorithm>
 #include <atomic>
