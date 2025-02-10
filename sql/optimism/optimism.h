76043e68c111a220a7d6545c2c099382bf2026aa
Morten T. R. Sunde <mtrsunde@outlook.com>
2025-02-10 16:09:39 +0100

    add broken explain acces-path stuff

--- sql/optimism/optimism.h
+++ sql/optimism/optimism.h
@@ -1,9 +1,21 @@
 #pragma once
 
 enum class OptimismFunc {
-    NONE,
-    LINEAR,
-    CLAMPED,
-    SIGMOID,
-    EXPONENTIAL
+  NONE, LINEAR, CLAMPED, SIGMOID, EXPONENTIAL 
 };
+
+inline const char *OptimismFuncToString(OptimismFunc optimismFunc) {
+  switch (optimismFunc) {
+    case OptimismFunc::NONE:
+      return "NONE";
+    case OptimismFunc::LINEAR:
+      return "LINEAR";
+    case OptimismFunc::CLAMPED:
+      return "CLAMPED";
+    case OptimismFunc::SIGMOID:
+      return "SIGMOID";
+    case OptimismFunc::EXPONENTIAL:
+      return "EXPONENTIAL";
+  }
+}
+
