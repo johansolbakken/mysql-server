c834d08a11f1e04d21cf5d93b120492c0cc9bf68
Johan Solbakken <johan.hvaler@gmail.com>
2025-02-10 09:38:10 +0100

    oohj: add set optimism level hint

--- sql/parse_tree_hints.cc
+++ sql/parse_tree_hints.cc
@@ -592,7 +592,6 @@ bool PT_hint_disable_optimistic_hash_join::do_contextualize(Parse_context *pc) {
 }
 
 bool PT_hint_set_optimism_level::do_contextualize(Parse_context *pc) {
-    *(char*)nullptr;
     pc->thd->optimism_level = m_level;
     return false;
 }
